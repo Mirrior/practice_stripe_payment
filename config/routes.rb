c31f36cd99bd312af889263ee32318466c8b9d22
Rustin Player <rjplayer@outlook.com>
2018-02-19 15:57:37 -0700

    Created charges controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'charges/new'
+
+  get 'charges/create'
+  resources :charges, only: [:new, :create]
   devise_for :users
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'posts#home'
+  root to: 'posts#index'
 end
