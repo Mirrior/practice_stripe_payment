157d517e338eb082ff676ca55519074142f37cfe
Rustin Player <rjplayer@outlook.com>
2018-09-06 13:39:07 -0600

    Integrated Payments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :payments, only: [:new, :create]
-  get 'payments-thanks', to: 'payments#thanks', as: 'payments_thanks'
+  get 'payment-thanks', to: 'payments#thanks', as: 'payment_thanks'
   resources :charges, only: [:new, :create]
   get 'thanks', to: 'charges#thanks', as: 'thanks'
   devise_for :users
